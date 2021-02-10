54f172def9ff3fd53451fa4b7a63f72169aed66c
Kaushal Kulkarni <kkulkarni@gocurrency.com>
2021-02-10 00:00:03 -0800

    add serverSideRendering

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,5 +1,5 @@
 import Layout from "../../components/layout";
-import { GetStaticPaths, GetStaticProps } from "next";
+import { GetStaticPaths, GetStaticProps, GetServerSideProps } from "next";
 import { getAllPostIds, getPostData } from "../../lib/posts";
 import Head from "next/head";
 import utilStyles from "../../styles/utils.module.scss";
@@ -21,19 +21,42 @@ export default function Post({ postData }) {
   );
 }
 
-export const getStaticPaths: GetStaticPaths = async () => {
-  const paths = getAllPostIds();
-  return {
-    paths,
-    fallback: false,
-  };
-};
+// /**
+//  * @note This function gets called at build time and lets you specify which paths you want to pre-render.
+//  */
+// export const getStaticPaths: GetStaticPaths = async () => {
+//   // Return a list of possible value for id. Our route is pages/[id]
+//   // { fallback: false } means other routes should result in 404.
+//   const paths = getAllPostIds();
+//   return {
+//     paths,
+//     fallback: false,
+//   };
+// };
 
-export const getStaticProps: GetStaticProps = async ({ params }) => {
-  const postData = await getPostData(params.id);
-  return {
-    props: {
-      postData,
-    },
+// /**
+//  * @note  This function gets called at build time and lets you pass fetched data to the page's props on pre-render.
+//  */
+// export const getStaticProps: GetStaticProps = async ({ params }) => {
+//   // Fetch necessary data for the blog post using params.id. If route is /posts/1, then params.id = 1
+//   console.log(params);
+//   const postData = await getPostData(params.id);
+//   return {
+//     props: {
+//       postData,
+//     },
+//   };
+// };
+
+/**
+ * 
+ * @note This function gets called at request time
+ */
+export const getServerSideProps: GetServerSideProps = async ({ params }) => {
+    const postData = await getPostData(params.id);
+    return {
+      props: {
+        postData,
+      },
+    };
   };
-};
